7408b615874956b5ebef22c46c6caec0bb63a927
guilherme-gjv <guijesus.info@gmail.com>
2023-08-04 22:57:24 -0300

    chore: movendo arquivos da pasta mylittlegarden para raiz. // removendo pasta duplicada

--- src/main/java/com/api/mylittlegarden/MylittlegardenApplication.java
+++ src/main/java/com/api/mylittlegarden/MylittlegardenApplication.java
@@ -1,4 +1,4 @@
-package com.api.mylittlegarden.mylittlegarden;
+package com.api.mylittlegarden;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/main/java/com/api/mylittlegarden/controllers/LabController.java
+++ src/main/java/com/api/mylittlegarden/controllers/LabController.java
@@ -1,4 +1,4 @@
-package com.api.mylittlegarden.mylittlegarden.controllers;
+package com.api.mylittlegarden.controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
